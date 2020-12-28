a8780919d3537f8a5154fd35ff41eed1baca6b2b
Ramón Alberto Escobar Madrid <31197406+ramonescobar@users.noreply.github.com>
2020-12-27 23:03:12 -0600

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ df2 =pd.read_csv("Net_Migration.csv", sep=';')
 df3=pd.read_csv('1022be19-c0ef-473b-9c38-f5de90877db0_Data.csv', sep=';')
 
 #App
-app=dash.Dash()
+app=dash.Dash(__name__)
 server=app.server
 
 Cols=list(df2)
